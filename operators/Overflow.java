74790e627276aff7955340e76c8f357560f2c1fe
CSapprenticeWT <838167398@qq.com>
2023-01-03 16:20:49 +0800

    添加注释

--- operators/Overflow.java
+++ operators/Overflow.java
@@ -8,7 +8,7 @@ public class Overflow {
   public static void main(String[] args) {
     int big = Integer.MAX_VALUE;
     System.out.println("big = " + big);
-    int bigger = big * 4; // 数据不对
+    int bigger = big * 4; // 数据不对 不是2147483647*4
     System.out.println("bigger = " + bigger);
   }
 }
